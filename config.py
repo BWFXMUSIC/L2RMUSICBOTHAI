743a4c4d835b99d725634c7f1a204d282b5ecd0b
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-12-18 11:59:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BWFXMUSIC/L2RMUSICBOTHAI",
+    "https://github.com/BWFXMUSIC/BWFTEST",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
